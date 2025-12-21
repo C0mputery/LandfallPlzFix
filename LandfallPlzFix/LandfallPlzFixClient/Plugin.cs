f1c8f8c46c72e31ca709bdd815936ba136706bcd
Computery <77170025+C0mputery@users.noreply.github.com>
2025-12-20 20:02:26 -0700

    Token Auth Fix

--- LandfallPlzFix/LandfallPlzFixClient/Plugin.cs
+++ LandfallPlzFix/LandfallPlzFixClient/Plugin.cs
@@ -1,14 +1,18 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
-namespace LandfallPlzFixClient
-{
+namespace LandfallPlzFixClient {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        private void Awake()
-        {
-            // Plugin startup logic
+    public class Plugin : BaseUnityPlugin { 
+        public new static ManualLogSource Logger;
+        
+        private void Awake() {
+            Logger = base.Logger;
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginInfo.PLUGIN_GUID);
+            Logger.LogInfo($"Applied patches.");
         }
     }
 }
