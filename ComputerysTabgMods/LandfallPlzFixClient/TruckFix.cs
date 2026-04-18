6e38eb3d618427a9419a48ad7c8ae3f0c3311739
Computery <77170025+C0mputery@users.noreply.github.com>
2026-04-17 22:53:31 -0400

    what the fuck

--- ComputerysTabgMods/LandfallPlzFixClient/TruckFix.cs
+++ ComputerysTabgMods/LandfallPlzFixClient/TruckFix.cs
@@ -2,32 +2,13 @@ using HarmonyLib;
 using UnityEngine;
 
 namespace LandfallPlzFixClient {
-    [HarmonyPatch(typeof(Dropper))]
-    public class DropperDesyncFixPatch {
-        private static float _flightStartTime;
-
-        [HarmonyPostfix]
-        [HarmonyPatch(nameof(Dropper.StartFlying))]
-        public static void StartFlyingPostfix() { _flightStartTime = Time.time; }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(nameof(Dropper.ClientInit))]
-        public static void ClientInitPostfix() { _flightStartTime = Time.time; }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(nameof(Dropper.SyncProgress))]
-        public static void SyncProgressPostfix(Dropper __instance, float newPlaneProgress) {
-            _flightStartTime = Time.time - (newPlaneProgress * __instance.totalFlyingTime);
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(nameof(Dropper.LateUpdate))]
-        public static void LateUpdatePostfix(Dropper __instance) {
-            if (__instance.isFlying) {
-                __instance.progress = (Time.time - _flightStartTime) / __instance.totalFlyingTime;
-                Vector3 vector = __instance.startPos + __instance.pathToTravel * __instance.progress;
-                __instance.dropperObject.position = vector;
-            }
+    [HarmonyPatch(typeof(WormSegment))]
+    public class WormSegmentPatch {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(WormSegment.Start))]
+        public static void StartPrefix(WormSegment __instance) {
+            Rigidbody rigidbody = __instance.GetComponent<Rigidbody>();
+            rigidbody.interpolation = RigidbodyInterpolation.None;
         }
     }
 }
